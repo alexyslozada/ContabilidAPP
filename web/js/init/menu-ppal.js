1dba5d17c64dca5eaa2d43c531a0fdb5fdb04e64
Alexys Lozada <alexyslozada@msn.com>
2015-12-15 10:22:45 -0500

    Se corrige la función de toggle menú

--- web/js/init/menu-ppal.js
+++ web/js/init/menu-ppal.js
@@ -3,9 +3,8 @@
 (function(){
     var menuppal = _.getID('menu-ppal'),
         menuprincipal = _.getID('menu-principal'),
-        menuppal_ele = menuppal.get(),
-        i = 0, max = menuppal_ele.childNodes.length,
-        hijo = null, link = null;
+        links = menuppal.get().getElementsByClassName('menu-ppal__link'),
+        i = 0, max = links.length, link = null;
 
     /* Clic en el menú principal */
     menuprincipal.click(function(){
@@ -14,14 +13,9 @@
 
     // Se asigna la función de clic a cada item del menú
     for(; i < max; i++){
-        hijo = menuppal_ele.childNodes[i];
-        if(hijo.nodeName.toLowerCase() === 'li' && hijo.hasChildNodes()){
-            link = hijo.childNodes[0];
-            if(link.nodeName.toLowerCase() === 'a'){
-                _.getElement(link).click(function(){
-                    menuppal.toggleClass('mostrar-menu-ppal');
-                });
-            }
-        }
+        link = links[i];
+        _.getElement(link).click(function(){
+            menuppal.toggleClass('mostrar-menu-ppal');
+        });
     }
 })();
